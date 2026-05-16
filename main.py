d5fcf81dff4b0fff0ceb4eb5567bf8aaf6225c10
Markus Sköld <marsko6647@gmail.com>
2026-05-16 22:01:09 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from machine import Pin, SPI
-from ssd1306 import SSD1306_SPI
-
-print("START")
+from ssd1309 import Display
 
 spi = SPI(
     0,
-    baudrate=1_000_000,
+    baudrate=100_000,
     polarity=0,
     phase=0,
     sck=Pin(18),
     mosi=Pin(19)
 )
 
-print("SPI OK")
-
-oled = SSD1306_SPI(
-    128,
-    64,
-    spi,
+oled = Display(
+    spi=spi,
+    cs=Pin(17),
     dc=Pin(16),
-    res=Pin(20),
-    cs=Pin(17)
+    rst=Pin(20),
+    width=128,
+    height=64,
+    flip=False
 )
 
-print("OLED OK")
-
-oled.fill(1)
-oled.show()
-
-print("DONE")
+oled.clear()
+oled.draw_text8x8(0, 0, "Hejsan allihopa")
+oled.present()
